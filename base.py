9f42b9060e351553018a4d537c9a454c26a7b926
yagmursato <yagmur_sato@windowslive.com>
2017-07-25 14:03:43 +0300

    Robot için database fonksiyonları eklendi

--- base.py
+++ base.py
@@ -1,10 +1,11 @@
 import pymongo
 import tweepy
+import database
+
 import d
 import psycopg2
 
-connection = psycopg2.connect("dbname='robotorhuman_db' user='postgres' host='192.168.31.247' password='qazwsx'")
-
+db = database.db_add()
 consumer_key = d.consumer_key
 consumer_secret = d.consumer_secret
 access_token = d.access_token
@@ -15,27 +16,51 @@ human = d.human
 giris = tweepy.OAuthHandler(consumer_key, consumer_secret)
 giris.set_access_token(access_token, access_token_secret)
 api = tweepy.API(giris)
-count = 0
 human_file = open("human.txt", "a")
 robot_file = open("robot.txt", "a")
 
-with open("human.txt","r") as f:
-    a = f.read()
-    human_dizi = a.split()
-    print(human_dizi)
+def readd(dosya):
+    with open(dosya,"r") as f:
+        a = f.read()
+        dizi = a.split()
+        return dizi
+def db_insert_human():
+    human_dizi=readd("human.txt")
+    count = 0
+    for i in range(len(human_dizi)):
+        for j in range(len(human_dizi)):
+            if i<j:
+                if(human_dizi[i] == human_dizi[j]):
+                    human_dizi[j] = None
+                    count += 1
+        if(human_dizi[i] is not None and human_dizi[i] is not " "):
+            human_dizi[i] = human_dizi[i].replace("'","")
+            db.insert_human(human_dizi[i],count+1)
+        count=0
 
-connection = connection.cursor()
-for i in range(len(human_dizi)):
-    for j in range(len(human_dizi)):
-        if i<j:
-            if(human_dizi[i] == human_dizi[j]):
-                human_dizi[j] = None
-                count += 1
-    if(human_dizi[i] is not None):
-        human_dizi[i] = human_dizi[i].replace("'","")
-        connection.execute("insert into human (word,count) VALUES ('{}', {}); COMMIT ;".format(human_dizi[i],count+1))
-    count=0
+def db_insert_robot():
+    dizi=readd("robot.txt")
+    count = 0
+    for i in range(len(dizi)):
+        for j in range(len(dizi)):
+            if i<j:
+                if(dizi[i] == dizi[j]):
+                    dizi[j] = None
+                    count += 1
+        if(dizi[i] is not None):
+            dizi[i] = dizi[i].replace("'","")
+            dizi[i] = dizi[i].replace('"', "")
+            dizi[i] = dizi[i].replace('?', "")
+            dizi[i] = dizi[i].replace('-', "")
+            dizi[i] = dizi[i].replace(',', "")
+            dizi[i] = dizi[i].replace('', "")
+            db.insert_robot(dizi[i],count+1)
+        count=0
 
+deneme = ["selam","iyi","ki","misim"]
+
+print(db.human_count("RT"))
+print(db.robot_count("Scam"))
 
 
 
