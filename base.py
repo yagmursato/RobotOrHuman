d16b724f7ed01bef9f16f94cb4712f1a279dc280
utkucanbykl <utkucanbykl@gmail.com>
2017-07-25 12:08:36 +0300

    Find count

--- base.py
+++ base.py
@@ -1,6 +1,10 @@
-import csv
+import pymongo
 import tweepy
 import d
+import psycopg2
+
+connection = psycopg2.connect("dbname='robotorhuman_db' user='postgres' host='192.168.31.247' password='qazwsx'")
+
 consumer_key = d.consumer_key
 consumer_secret = d.consumer_secret
 access_token = d.access_token
@@ -11,10 +15,29 @@ human = d.human
 giris = tweepy.OAuthHandler(consumer_key, consumer_secret)
 giris.set_access_token(access_token, access_token_secret)
 api = tweepy.API(giris)
+count = 0
+human_file = open("human.txt", "a")
+robot_file = open("robot.txt", "a")
 
-for i in range(len(human)):
-    twitler = api.user_timeline(screen_name = human[i], count=100)
-    with open('deneme.csv', 'a') as f:
-        writer = csv.writer(f)
-        for i in range(100):
-            writer.writerow([twitler[i].text,True])
+with open("human.txt","r") as f:
+    a = f.read()
+    human_dizi = a.split()
+    print(human_dizi)
+
+connection = connection.cursor()
+for i in range(len(human_dizi)):
+    for j in range(len(human_dizi)):
+        if i<j:
+            if(human_dizi[i] == human_dizi[j]):
+                human_dizi[j] = None
+                count += 1
+    if(human_dizi[i] is not None):
+        human_dizi[i] = human_dizi[i].replace("'","")
+        connection.execute("insert into human (word,count) VALUES ('{}', {}); COMMIT ;".format(human_dizi[i],count+1))
+    count=0
+
+
+
+
+
+
